ff062276d6f7417c28939283817ae4462c029801
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-24 17:15:49 +0530

    updationcode

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ set :ssh_options,     { :forward_agent => true }
 set :rails_env,       "production"
 set :normalize_asset_timestamps, false
 
+default_environment["RAILS_ENV"] = 'production'
 
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -49,20 +50,20 @@ namespace :deploy do
    run "cp #{shared_path}/config/database.yml #{current_path}/config/database.yml"
    run "bundle install"
  end
-  #desc "Zero-downtime restart of Unicorn"
-  #task :restart, :except => { :no_release => true } do
-   # run "kill -s USR2 `cat /tmp/unicorn.singlemessaging.pid`"
-  #end
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -9 USR2 `cat /tmp/unicorn.singlemessaging.pid`"
+  end
 
-  #desc "Start unicorn"
-  #task :start, :except => { :no_release => true } do
-   # run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
-  #end
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
 
-  #desc "Stop unicorn"
-  #task :stop, :except => { :no_release => true } do
-   # run "kill -s QUIT `cat /tmp/unicorn.singlemessaging.pid`"
-  #end  
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -9 QUIT `cat /tmp/unicorn.singlemessaging.pid`"
+  end  
  desc "Deploy with migrations"
  task :long do
    transaction do
@@ -76,6 +77,11 @@ namespace :deploy do
    web.enable
    cleanup
  end
+   task :update do
+    transaction do
+      update_code
+    end
+  end
   desc "Update the deployed code."
   task :update_code, :except => { :no_release => true } do
     run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
@@ -90,6 +96,7 @@ namespace :deploy do
     migrate
     restart
   end
+  
  desc "Run cleanup after long_deploy"
  task :after_deploy do
    cleanup
