175a230a726c96d316b47b343638179bce2f98a0
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-09-10 16:16:22 +0530

    changingjquery-ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,3 @@
 //= require jquery
 //= require jquery-ui
-function add_fields(link, association, content) {
- alert("called")
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
- 
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
-}
 
